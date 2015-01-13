ec2f66be4799a2a0fa00b3830091d29dcb661dc6
Mike Graves <mgraves@mit.edu>
2015-01-13 16:27:55 -0500

    Actually use the fix

--- manage.py
+++ manage.py
@@ -8,10 +8,15 @@ manager = Manager(create_app())
 
 @manager.command
 def test():
-    import unittest
-    from colour_runner.runner import ColourTextTestRunner
-    suite = unittest.defaultTestLoader.discover('tests')
-    ColourTextTestRunner().run(suite)
+    try:
+        import unittest2
+        suite = unittest2.defaultTestLoader.discover('tests')
+        unittest2.TextTestRunner().run(suite)
+    except:
+        import unittest
+        from colour_runner.runner import ColourTextTestRunner
+        suite = unittest.defaultTestLoader.discover('tests')
+        ColourTextTestRunner().run(suite)
 
 if __name__ == '__main__':
     manager.run()
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import unittest
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 from webtest import TestApp
 from kepler.app import create_app
 from kepler.extensions import db
 from kepler.settings import TestConfig
+
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
