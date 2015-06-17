49644511b3d11595cfe594db3e2cb2b988544519
Mike Graves <mgraves@mit.edu>
2015-06-17 12:45:21 -0400

    Use Job id in URL

--- tests/test_views.py
+++ tests/test_views.py
@@ -17,12 +17,12 @@ def create_job():
 
 class TestJob(object):
     def testJobCreationReturns201OnSuccess(self, testapp, create_job):
-        r = testapp.post('/job/', {'uri': 'foobar', 'type': 'shapefile'},
+        r = testapp.post('/jobs/', {'uri': 'foobar', 'type': 'shapefile'},
                          upload_files=[('file', 'tests/data/bermuda.zip')])
         assert r.status_code == 201
 
     def testJobCreationRunsJob(self, testapp, create_job):
-        testapp.post('/job/', {'uri': 'foobar', 'type': 'shapefile'},
+        testapp.post('/jobs/', {'uri': 'foobar', 'type': 'shapefile'},
                      upload_files=[('file', 'tests/data/bermuda.zip')])
         assert create_job.call_count == 1
 
@@ -30,34 +30,27 @@ class TestJob(object):
         create_job.side_effect = Exception
         testapp.app.debug = False
         testapp.app.config['PROPAGATE_EXCEPTIONS'] = False
-        r = testapp.post('/job/', {'uri': 'foobar', 'type': 'shapefile'},
+        r = testapp.post('/jobs/', {'uri': 'foobar', 'type': 'shapefile'},
                          upload_files=[('file', 'tests/data/bermuda.zip')],
                          expect_errors=True)
         assert r.status_code == 500
 
     def testJobCreationReturns415OnUnsupportedFormat(self, testapp, db):
-        r = testapp.post('/job/', {'uri': 'foobar', 'type': 'warez'},
+        r = testapp.post('/jobs/', {'uri': 'foobar', 'type': 'warez'},
                          expect_errors=True)
         assert r.status_code == 415
 
-    def testJobRetrievalReturns200OnSuccess(self, testapp, db):
-        job = Job(item=Item(uri=u'TestJob'))
+    def testJobRetrievalShowsJob(self, testapp, db):
+        job = Job(item=Item(uri=u'TestJob'), status='COMPLETED')
         db.session.add(job)
-        r = testapp.get('/job/%s' % job.item.uri)
-        assert r.status_code == 200
-
-    def testJobRetrievalReturnsMostRecentJob(self, testapp, db):
-        item = Item(uri=u'TestJob')
-        db.session.add(Job(item=item, time=datetime(2001, 1, 1)))
-        db.session.add(Job(item=item, status="COMPLETED",
-                           time=datetime(2002, 1, 1)))
-        r = testapp.get('/job/TestJob')
-        assert r.json['status'] == 'COMPLETED'
+        db.session.commit()
+        r = testapp.get('/jobs/%d' % job.id)
+        assert 'COMPLETED' in r.text
 
     def testJobListReturnsJobs(self, testapp, db):
         db.session.add(Job(item=Item(uri=u'FOO')))
         db.session.add(Job(item=Item(uri=u'BAR')))
-        r = testapp.get('/job/')
+        r = testapp.get('/jobs/')
         assert 'FOO' in r.text
         assert 'BAR' in r.text
 
@@ -66,6 +59,6 @@ class TestJob(object):
         db.session.add(Job(item=item, time=datetime(2001, 1, 1)))
         db.session.add(Job(item=item, time=datetime(2002, 1, 1),
                            status="COMPLETED"))
-        r = testapp.get('/job/')
+        r = testapp.get('/jobs/')
         assert 'COMPLETED' in r.text
         assert 'PENDING' not in r.text
