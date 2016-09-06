10c3e7d3da3f54bc960a5aef597ada2e45f64dad
Mike Graves <mgraves@mit.edu>
2016-09-06 13:27:41 -0400

    Add pagination for jobs
    Closes #156.

--- kepler/job/views.py
+++ kepler/job/views.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 from flask.views import View
 from flask import request, render_template
+from flask_paginate import Pagination
 from sqlalchemy.sql import func
 from sqlalchemy import and_
 
@@ -11,30 +12,49 @@ from kepler.extensions import db
 
 
 class JobView(View):
+    PER_PAGE = 15
+
     def dispatch_request(self, *args, **kwargs):
         if request.method == 'GET':
             if request.endpoint.endswith('.index'):
                 return self.list()
+            elif request.endpoint.endswith('.status'):
+                return self.filtered(*args, **kwargs)
             return self.show(*args, **kwargs)
 
     def list(self):
-        pending = []
-        completed = []
-        failed = []
-        sub_q = db.session.query(Job.item_id, func.max(Job.time).label('time')).\
+        sub_q = db.session.query(Job.item_id, func.max(Job.time).
+                                 label('time')).\
             group_by(Job.item_id).subquery()
         q = db.session.query(Job).\
             join(sub_q, and_(Job.item_id == sub_q.c.item_id,
-                             Job.time == sub_q.c.time)).\
-            order_by(Job.time.desc())
-        for job in q.filter(Job.status == 'PENDING'):
-            pending.append(job)
-        for job in q.filter(Job.status == 'COMPLETED'):
-            completed.append(job)
-        for job in q.filter(Job.status == 'FAILED'):
-            failed.append(job)
+                             Job.time == sub_q.c.time))
+        pending = q.filter(Job.status == 'PENDING').\
+            order_by(Job.time.desc()).limit(self.PER_PAGE)
+        completed = q.filter(Job.status == 'COMPLETED').\
+            order_by(Job.time.desc()).limit(self.PER_PAGE)
+        failed = q.filter(Job.status == 'FAILED').\
+            order_by(Job.time.desc()).limit(self.PER_PAGE)
         return render_template('job/index.html', pending=pending,
                                completed=completed, failed=failed)
+
+    def filtered(self, status, page=1):
+        offset = max(page-1, 0) * self.PER_PAGE
+        sub_q = db.session.query(Job.item_id, func.max(Job.time).
+                                 label('time')).\
+            group_by(Job.item_id).subquery()
+        jobs_q = db.session.query(Job).\
+            join(sub_q, and_(Job.item_id == sub_q.c.item_id,
+                             Job.time == sub_q.c.time,
+                             Job.status == status.upper()))
+        jobs = jobs_q.\
+            order_by(Job.time.desc()).\
+            limit(self.PER_PAGE).\
+            offset(offset)
+        pagination = Pagination(page=page, total=jobs_q.count(),
+                                per_page=self.PER_PAGE, bs_version=3)
+        return render_template('job/status.html', jobs=jobs, status=status,
+                               pagination=pagination)
 
     def show(self, id):
         job = Job.query.get_or_404(id)
@@ -42,7 +62,12 @@ class JobView(View):
 
     @classmethod
     def register(cls, app, endpoint, url):
+        status = '{}<any(completed,failed,pending):status>/'.format(url)
         view_func = cls.as_view(endpoint)
         app.add_url_rule(url, 'index', methods=['GET'], view_func=view_func)
-        app.add_url_rule('%s<path:id>' % url, 'resource', methods=['GET'],
+        app.add_url_rule(status, 'status', methods=['GET'],
+                         view_func=view_func)
+        app.add_url_rule('{}<int:page>/'.format(status), 'status',
+                         methods=['GET'], view_func=view_func)
+        app.add_url_rule('%s<int:id>' % url, 'resource', methods=['GET'],
                          view_func=view_func)
